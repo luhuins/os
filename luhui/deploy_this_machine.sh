281a933ce1b460cce265de53ee3966b4941d532f
luhui <luhuins@163.com>
2021-04-20 19:51:07 +0800

    luhui/dotfile: 添加用于部署机器的脚本
    Signed-off-by: luhui <luhuins@163.com>

---
+++ luhui/deploy_this_machine.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -e
+set -u
+
+CWD=$(pwd)
+
+#
+# root user:
+#
+
+export GUIX_SOURCE_GIT_REPO="/srv/code/git/guix"
+
+# update guix source code
+cd ${GUIX_SOURCE_GIT_REPO}/
+git pull gnu master
+git fetch gnu keyring:keyring
+cd ${CWD}/
+
+# update guix
+guix pull --url=${GUIX_SOURCE_GIT_REPO} --branch=master 
+hash guix
+
+# need load path
+export GUIX_USER_CNFIG_GIT_REPO="/srv/code/git/linux-os"
+export GUIX_BUILD_OPTIONS="-L${GUIX_USER_CNFIG_GIT_REPO}"
+# update guix system
+guix system reconfigure /etc/config.scm 
+
+
+#
+# normal users:
+#
+cd /home
+for i in *
+do
+	if [ -e ${i}/.deploy_this_user.sh ]
+	then
+		su - ${i} -c "/bin/sh deploy_this_user.sh"
+	else
+		echo "user ${i} not have deploy_this_user.sh"
+	fi
+done
---
+++ luhui/dotfile/guix/.deploy_this_user.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+source ${HOME}/.bash_profile
+
+set -e
+set -u
+
+CWD=$(pwd)
+
+export GUIX_SOURCE_GIT_REPO="/srv/code/git/guix"
+guix pull --url="${GUIX_SOURCE_GIT_REPO}" --branch=master
+hash guix
+
+export GUIX_USER_CONFIG_GIT_REPO="/srv/code/git/linux-os"
+export GUIX_BUILD_OPTIONS="-L${GUIX_USER_CONFIG_GIT_REPO}"
+export USER_NAME=$(id -n -u)
+
+cd ${GUIX_USER_CONFIG_GIT_REPO}/luhui/manifests/
+guix build -m ${USER_NAME}.scm
+guix package -m ${USER_NAME}.scm
+cd ${CWD}
