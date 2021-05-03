8bb018760119ac9ef4f13b7b58c31580b8babc2b
luhui <luhuins@163.com>
2021-05-03 18:12:03 +0800

    luhui/services: 添加thinkpad-fan服务
    Signed-off-by: luhui <luhuins@163.com>

--- luhui/services/notebook.scm
+++ luhui/services/notebook.scm
@@ -1,11 +1,14 @@
 (define-module (luhui services notebook)
   #:use-module (gnu services)
   #:use-module (gnu services shepherd)
+  #:use-module (gnu services linux)
   #:use-module (guix gexp)
   #:use-module (guix records)
   #:use-module (gnu packages)
   #:export (brightness-configuration
-            brightness-service-type))
+            brightness-service-type
+            thinkpad-fan-configuration
+            thinkpad-fan-service-type))
 
 
 (define-record-type* <brightness-configuration>
@@ -78,3 +81,45 @@
         (modules `((rnrs io ports)
                    ,@%default-modules)))))
    (description "save & restore on service start/stop")))
+
+(define %thinkpad-fan-device-config
+  `("modprobe.d/thinkpad-fan.conf"
+    ,(plain-file "thinkpad-fan.conf"
+                 "options thinkpad_acpi fan_control=1")))
+
+(define-record-type* <thinkpad-fan-configuration>
+  thinkpad-fan-configuration make-thinkpad-fan-configuration
+  thinkpad-fan-configuration?
+  (speed thinkpad-fan-configuration-speed
+         (default "auto")))
+
+(define thinkpad-fan-shepherd-service
+  (lambda (config)
+    (let* ((speed (thinkpad-fan-configuration-speed config))
+           (level-control-file ; 控制风扇的文件
+            "/proc/acpi/ibm/fan"))
+      (shepherd-service
+       (documentation "set up thinkpad's fan")
+       (provision (list 'thinkpad-fan))
+       (start
+        #~(lambda _
+            (call-with-output-file #$level-control-file
+              (lambda (level-control-port)
+                (format level-control-port speed)))))
+       (modules `((rnrs io ports)
+                  ,@%default-modules))))))
+
+(define thinkpad-fan-service-type
+  (service-type
+   (name 'thinkpad-fan)
+   (extensions
+    (list
+     (service-extension kernel-module-loader-service-type
+                        (const (list "thinkpad_acpi")))
+     (service-extension etc-service-type
+                        (const (list %thinkpad-fan-device-config)))
+     (service-extension shepherd-root-service-type
+                        (compose list thinkpad-fan-shepherd-service))))
+   (description "set up thinkpad's fan")
+   (default-value (thinkpad-fan-configuration))))
+
